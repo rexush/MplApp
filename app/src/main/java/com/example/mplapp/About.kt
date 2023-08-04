109b1df3a7c8127772d67543915e74253d76d2c1
Husni Sabar <husnisabar592@gmail.com>
2023-08-04 14:02:35 +0700

    nambahin angka 1 di about

--- app/src/main/java/com/example/mplapp/About.kt
+++ app/src/main/java/com/example/mplapp/About.kt
@@ -10,7 +10,7 @@ class About: AppCompatActivity() {
         setContentView(R.layout.activity_about)
 
         val actionBar = supportActionBar
-        actionBar!!.title = "About"
+        actionBar!!.title = "About1"
         actionBar.setDisplayHomeAsUpEnabled(true)
     }
 
